10cf2a4d958556216c05cc384ba7567ddc9e60f1
Ne1t <2015vlad08@gmail.com>
2024-09-23 12:38:49 +0400

    Update MessageClientProgram.cs

--- MessageClient/MessageClientProgram.cs
+++ MessageClient/MessageClientProgram.cs
@@ -1,18 +1,15 @@
-﻿using System;
-using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
 
 class MessageClientProgram
 {
     static async Task Main(string[] args)
     {
         var baseUrl = "http://localhost:5273";
-        using var client = new HttpClient();
+        using var client = new HttpClient(); //HttpClient() - класс для запросов http - GET, POST...
 
         while (true)
         {
-            Console.Write("Сообщение ('exit' - выход): ");
+            Console.Write("Сообщение:'exit' - выход: ");
             var message = Console.ReadLine();
 
             if (message?.ToLower() == "exit")
@@ -20,19 +17,20 @@ class MessageClientProgram
                 break;
             }
 
-            var content = new StringContent(message, Encoding.UTF8, "text/plain");
-            var response = await client.PostAsync($"{baseUrl}/message", content);
-
-            if (response.IsSuccessStatusCode)
+            //POST - запрос
+            var content = new StringContent(message, Encoding.UTF8, "text/plain"); //StringContent отправляет,упаковывает строку (Cmd text, Кодировка, Тип текста)
+            var response = await client.PostAsync($"{baseUrl}/message", content); //PostAsync метод класса HttpClient, для выполнения post запроса
+            //baseUrl/message - базовый URL сервера/путь , content - содержит обьект StringContent строковое сообщение из Cmd
+            if (response.IsSuccessStatusCode) //свойство обьекта указывающее был ли запрос успешным
             {
-                var responseMessage = await response.Content.ReadAsStringAsync();
+                var responseMessage = await response.Content.ReadAsStringAsync(); // обьект_запрос.свойство_обьекта.метод читает содержимое Content
                 Console.WriteLine($"Ответ сервера: {responseMessage}");
             }
             else
             {
                 Console.WriteLine($"Ошибка: {response.StatusCode}");
             }
-
+            //GET - запрос
             var nameResponse = await client.GetAsync($"{baseUrl}/name");
             if (nameResponse.IsSuccessStatusCode)
             {
