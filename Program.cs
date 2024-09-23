02bcd00e2ba8a346d83c4898459d467549352b8d
Ne1t <2015vlad08@gmail.com>
2024-09-23 12:38:26 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,3 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Hosting;
-using System.Collections.Generic;
-
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://localhost:5273");
 
@@ -11,34 +6,33 @@ var app = builder.Build();
 
 List<string> messages = new List<string>();
 
-
+//MapGet() даёт нам понять какой маршрут нужно выполнить /message или /name
 app.MapGet("/", async context =>
 {
-    context.Response.ContentType = "text/html; charset=utf-8";
+    context.Response.ContentType = "text/html; charset=utf-8"; // text/html - это тип ответа сервера, utf-8 - кодировка для коректного отображения текста
 
-    
     string response = "<h1>Сообщения:</h1><ul>";
     foreach (var message in messages)
     {
         response += $"<h3>{message}</h3>";
     }
     response += "</ul>";
-    await context.Response.WriteAsync(response);
+    await context.Response.WriteAsync(response); // отправка готовой html страницы в ответ на запрос клиента 
 });
 
 // POST /message
 app.MapPost("/message", async (HttpContext context) =>
 {
-    using (var reader = new StreamReader(context.Request.Body))
+    using (var reader = new StreamReader(context.Request.Body)) //StreamReader - читает данные из потока, context.Request.Body - поток данных клиента
     {
-        string message = await reader.ReadToEndAsync();
+        string message = await reader.ReadToEndAsync(); //ReadToEndAsync() - используется для асинхронного четения всего содержимого потока до самого конца
         messages.Add(message);
         Console.WriteLine($"Получено сообщение: {message}");
-        await context.Response.WriteAsync($"Сообщение получено: {message}");
+        await context.Response.WriteAsync($"Сообщение получено: {message}"); //отправка ответа сервера к клиенту
     }
 });
 
 // GET /name
-app.MapGet("/name", () => "MyServer");
+app.MapGet("/name", () => "MyServer"); // возвращает обычную строку
 
-app.Run();
+app.Run(); //запускает сервер
