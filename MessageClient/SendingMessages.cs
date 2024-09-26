d657c11815d374e120ef1f3ab45a69550df51bf1
user <2015vlad08@gmail.com>
2024-09-26 14:05:12 +0400

    Rest

--- MessageClient/SendingMessages.cs
+++ MessageClient/SendingMessages.cs
@@ -4,48 +4,46 @@ using System.Text;
 
 class SendingMessages
 {
-
     // Отправка сообщения на другой сервер
     public async Task<string> SendMessageToExternalServer(string message)
     {
-
         using (var client = new HttpClient())
         {
-            var baseUrl = "http://172.29.9.90:3400/message"; // Путь / адрес
+            var baseUrl = "http://172.29.9.90:3400/message"; // URL внешнего сервера
             Console.WriteLine($"Отправка сообщения на {baseUrl}: {message}");
 
-            var messageModel = new { message = message }; // messageModel объект для отправки
+            var messageModel = new { message = message };
             var json = JsonSerializer.Serialize(messageModel);
 
-            var content = new StringContent(json, Encoding.UTF8, "application/json"); // JSON в StringContent
-            // POST запрос
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
             var response = await client.PostAsync(baseUrl, content);
 
-            if (response.IsSuccessStatusCode) // Проверка запроса
+            if (response.IsSuccessStatusCode)
             {
-                Console.WriteLine($"Сообщение доставлено: {baseUrl}"); // URL при отправке
-                //Console.WriteLine($"Сообщение номер {i}");
-                return baseUrl;
+                var externalResponse = await response.Content.ReadAsStringAsync(); // Читаем ответ внешнего сервера
+                Console.WriteLine($"Сообщение доставлено: {externalResponse}");
+                return externalResponse; // Возвращаем ответ для отображения на сайте
             }
             else
             {
                 Console.WriteLine($"Ошибка доставки сообщения: {response.StatusCode}");
-                return response.StatusCode.ToString(); // Код ошибки как строка
+                return $"Ошибка доставки сообщения: {response.StatusCode}";
             }
-
         }
     }
 
+    // Повторная отправка сообщений
     public async Task<double> ResendingMessages(Func<Task> SendMessageToExternalServer)
     {
-        int massageCount = 1000; var stopwatch = Stopwatch.StartNew(); double Minuts;
+        int massageCount = 4; double Minuts;
         await Task.Delay(1); //задержка в 1 миллсек.
-        for(int i = 0; i < massageCount; i++)
+        var stopwatch = Stopwatch.StartNew();
+        for (int i = 0; i < massageCount; i++)
         {
             await SendMessageToExternalServer();
         }
         stopwatch.Stop(); // Остановка таймера
-        Minuts = stopwatch.Elapsed.TotalSeconds / 60;
+        Minuts = stopwatch.Elapsed.TotalSeconds / 60; //stopwatch.Elapsed - получает общее затраченное время
         Console.WriteLine($"Отправленно {massageCount} в минутах: {Minuts}");
         return Minuts;
     }
